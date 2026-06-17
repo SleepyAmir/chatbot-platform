becbfaf13f25315b1d03c646e086f8c40fb29f11
SleepyAmir <amir.mhe83@gmail.com>
2026-06-17 17:31:35 +0330

    BaseEntities

--- src/main/java/com/example/platform/PlatformApplication.java
+++ src/main/java/com/example/platform/PlatformApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.flyway.autoconfigure.FlywayAutoConfiguration;
 
-@SpringBootApplication(exclude = { FlywayAutoConfiguration.class })
+@SpringBootApplication
 public class PlatformApplication {
 
 	public static void main(String[] args) {
