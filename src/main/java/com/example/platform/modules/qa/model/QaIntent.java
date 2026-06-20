e49ae20fe10f7325811a35ba9fd0aab1b54a103f
mobinaRahi <marinacoda21@gmail.com>
2026-06-20 16:00:20 +0330

    add qa-module Model

---
+++ src/main/java/com/example/platform/modules/qa/model/QaIntent.java
@@ -0,0 +1,28 @@
+package com.example.platform.modules.qa.model;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "qa_intents")
+@IdClass(QaIntentId.class)
+public class QaIntent {
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "qa_id", nullable = false)
+    private QaPair qaPair;
+
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "intent_id", nullable = false)
+    private Intent intent;
+}
+
